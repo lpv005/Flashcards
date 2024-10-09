c1ebce9f49ba8a73451a56bc44df84c8aba424ea
lpv005 <00001113768125sp@al.educacao.sp.gov.br>
2024-10-09 08:15:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Marechal Deodoro da Fonseca'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo?',
+    'O continente asiático é o maior do mundo, em termos de área e população'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a raiz quadrada de 144?',
+    'A raiz quadrada de 144 é 12'
+)
+
+criaCartao(
+    'Biologia',
+    'Do que as plantas precisam para realizar a fotossíntese?',
+    'Precisam de: Luz solar, água e dióxido de carbono'
+)
